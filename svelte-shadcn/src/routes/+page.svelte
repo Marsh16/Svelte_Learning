bfb015bb668e9d6d06773fd03acc51040d478ba7
Marsh16 <marshaalexis16@gmail.com>
2025-10-01 17:35:41 +0700

    chore: trying oi\ut shadcn ui

---
+++ svelte-shadcn/src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import Button from "$lib/components/ui/button/button.svelte";
+  import * as Tooltip from "$lib/components/ui/tooltip/index.js";
+</script>
+
+<div class="flex items-center justify-center h-screen bg-gray-200">
+  <Tooltip.Provider>
+    <Tooltip.Root>
+      <Tooltip.Trigger>
+        <Button>Hover</Button>
+      </Tooltip.Trigger>
+      <Tooltip.Content>
+        <p>Add to library</p>
+      </Tooltip.Content>
+    </Tooltip.Root>
+  </Tooltip.Provider>
+</div>
